a27a5fb5b2b634b5e2a7019cd3717670a59811e0
Flynn <74693969+flynnsol@users.noreply.github.com>
2024-06-15 00:13:33 -0500

    update

--- projects/FactorialCalculator/src/main.rs
+++ projects/FactorialCalculator/src/main.rs
@@ -2,8 +2,8 @@ use std::io;
 
 fn main() {
     println!("Enter your number:");
-    let mut number: i128 = get_user_input();
-    let mut answer: i128 = number;
+    let mut number: u128 = get_user_input();
+    let mut answer: u128 = number;
 
     while number > 1 {
         number -= 1;
@@ -11,19 +11,18 @@ fn main() {
     }
 
     println!("Answer: {}", answer);
-
 }
 
-fn get_user_input() -> i128 {
+fn get_user_input() -> u128 {
     let mut user_input = String::new();
-    let mut user_input_num: i128 = 0;
+    let mut user_input_num: u128 = 0;
 
     io::stdin().read_line(&mut user_input).expect("Failed to read line");
     user_input = user_input.trim().to_string();
 
-    match user_input.parse::<i128>() {
+    match user_input.parse::<u128>() {
         Ok(n) => {
-            if n > 0 && n < 34 {
+            if n > 0 && n < 35 {
                 user_input_num = n;
             } else {
                 println!("Number must be larger than 0 and Less than 34!");
